4a6eb129d757036abfb92cf097a22e4070bfa060
Doug Miller <Djmiller1717@gmail.com>
2020-11-22 18:27:50 -0500

    server index.js setup

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const express = require('express');
 
 const app = express();
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-const { db } = require('./db');
-const api = require('./api');
-const authMiddleware = require('./middleware/authentication');
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(cookieParser());
-app.use(authMiddleware);
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -25,23 +16,17 @@ app.use((req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err, err.stack);
   res.status(err.status || 500);
-  res.send(`something wrong: ${err.message}`);
+  res.send(`Something wrong: ${err.message}`);
 });
 
-async function init() {
-  try {
-    console.log('syncing');
-    await db.sync();
-    const PORT = process.env.PORT || 3000;
-    await app.listen(PORT, () => {
-      console.log(`Listening at http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
+const init = () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+  });
+};
 
 init();
