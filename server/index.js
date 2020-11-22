d3472100f51719df837f2ff0442f86e44b9d7e10
Doug Miller <Djmiller1717@gmail.com>
2020-11-21 22:41:52 -0500

    server setup and basic front-end file structure

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const { db } = require('./db');
+const api = require('./api');
+const authMiddleware = require('./middleware/authentication');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(authMiddleware);
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  console.error(err, err.stack);
+  res.status(err.status || 500);
+  res.send(`something wrong: ${err.message}`);
+});
+
+async function init() {
+  try {
+    console.log('syncing');
+    await db.sync();
+    const PORT = process.env.PORT || 3000;
+    await app.listen(PORT, () => {
+      console.log(`Listening at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
