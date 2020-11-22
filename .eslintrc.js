8e86a0caa5cc9a2760e199ccb67ba5dba2695056
Doug Miller <Djmiller1717@gmail.com>
2020-11-21 20:42:35 -0500

    linter added

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+  },
+};
