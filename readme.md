de0d2f24865322e247e2cc74e9183758f7fb87f7
Mark Guinn <mxavier927@gmail.com>
2020-11-22 16:13:23 -0500

    added client and server directories, server index.js file  and run scripts

--- readme.md
+++ readme.md
@@ -1,6 +1,10 @@
 ##### KeepDrawing!
 A drawing game by Jeff Garber, Mark Guinn, Oscar Sanchez, and Doug Miller
 
+To run locally:
+npm install
+npm run start
+
 This game uses .....
 
 
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err, err.stack);
+  res.status(err.status || 500);
+  res.send(`Something wrong: ${err.message}`);
+});
+
+const init = () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+  });
+};
+
+init();
