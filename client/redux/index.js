5eb8d4320a046dac24909ccab3828372a406ac23
Doug Miller <Djmiller1717@gmail.com>
2020-11-22 19:21:34 -0500

    webpack added/configured

---
+++ client/redux/index.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import axios from 'axios';
+import thunkMiddleware from 'redux-thunk';
+
+const appReducer = combineReducers({});
+
+const middleware = [
+  thunkMiddleware.withExtraArgument({ axios }),
+];
+
+export default createStore(
+  appReducer,
+  applyMiddleware(...middleware),
+);
