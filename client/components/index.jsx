11835d2445587234a6c039709167d17846319e07
Doug Miller <Djmiller1717@gmail.com>
2020-11-22 19:23:58 -0500

    frontend structure setup

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -3,14 +3,14 @@ import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
-import handTrack from './handTrack';
+// import Home from './Home';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          {/* <Route exact path="/" component={handTrack} /> */}
+          {/* <Route exact path="/" component={Home} /> */}
         </Switch>
       </Router>
     );
