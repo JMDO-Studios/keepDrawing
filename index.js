ccd3a913645f57a78360260cf9ba37bb54b35741
Oscar Sanchez <00oscarsanchez00@gmail.com>
2020-11-23 11:10:42 -0600

    sockets listening on server and client

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const app = express();
 
 const path = require('path');
 
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http);
+
+
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+io.on('connection', (socket) => {
+  //once the client/site is open this console message should appear
+  console.log('a user is connected');
+  //the following line listens for 'message' event type and the function(console message) allows testing connectivity for now
+  socket.on('message', (message)=>{
+    console.log(message)
+  })
+})
+
 
 app.use((req, res, next) => {
   const err = new Error('Not found');
@@ -24,7 +40,7 @@ app.use((err, req, res, next) => {
 
 const init = () => {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  http.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
   });
 };
