5a6a89f2bdb011232dd6d1f41cc517645a5177a5
Mark Guinn <markguinn@Marks-MacBook-Pro.local>
2020-12-12 10:34:07 -0500

    defined twilio state components

--- client/twilio/index.js
+++ client/twilio/index.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class TwilioChat extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      token: '',
+      socket: this.props,
+    };
+    this.joinTeam = this.joinTeam.bind(this);
+  }
+
+  joinTeam() {
+    const playerDetails = {
+      identity: socket.name,
+      room: socket.teamName,
+    };
+    const { data } = await axios.post('twilio/token', playerDetails);
+  }
+
+  render() {
+    return (
+      <div />
+    )
   }
 }
-
-
